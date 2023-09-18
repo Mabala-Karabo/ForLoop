9b56fa48965b5a81d0f22b72f259ef868c8bad8a
Mabala-Karabo <mabalakarabo25@gmail.com>
2023-09-18 16:56:00 +0200

    Add project files.

---
+++ ForLoop/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim i As Integer = 1
+
+        For i = 0 To 10 'for loop i starts from zero to 10
+            MsgBox("This is a for loop where i  value = " & i)
+        Next
+    End Sub
+End Class
